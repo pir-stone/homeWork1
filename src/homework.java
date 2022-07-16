bf73b5ac201ca08424d1773810e9feab66ecb314
Petr <lpp-@mail.ru>
2022-07-16 20:35:57 +0400

    задача 2

--- src/homework.java
+++ src/homework.java
@@ -1,5 +1,20 @@
 public class homework {
     public static void main(String[] args) {
-        int number = 2;
+//        задача 1
+
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+        String result = hi.trim() + world.toLowerCase() + newLine;
+        String newresult = result.repeat(3);
+        System.out.println(newresult);
+
+//        задача 2
+
+    double lenth = 1.82;
+    int weight = 90;
+    double bodyIndex = weight / (lenth * lenth);
+        System.out.println(bodyIndex + " kg/m2");
+
     }
 }
