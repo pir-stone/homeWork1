d6b98ecdf1c3cbcfda72e7ba55307093dc1da67b
Petr <lpp-@mail.ru>
2022-07-16 21:22:57 +0400

    задача 1

--- src/homework.java
+++ src/homework.java
@@ -14,7 +14,29 @@ public class homework {
     double lenth = 1.82;
     int weight = 90;
     double bodyIndex = weight / (lenth * lenth);
-        System.out.println(bodyIndex + " kg/m2");
+        System.out.println(bodyIndex + " kg/m2"+ '\n');
+
+        // задача 3
+        //Создать из массива букв a,b,c,d,e, строку,вывести на экран , поменять в массиве 4 букву по счету на h,
+        //и снова создать строку, вывести на экран
+        //Ожидаемый результат:
+        //abcde
+        //abche
+        char[] array = new char[]{'a','b','c','d','e'};
+        System.out.println(array);
+        array[3] = 'h';
+        System.out.println(array);
+
+//        продвинутый
+//        задача 1
+        //Произвести преобразование текста "234" в число типа int и прибавить к этому числу
+        // длину строки "some_text"
+        String text = "234";
+        String text2 = "some_text";
+        int answer = Integer.parseInt(text);
+        System.out.println(answer + text2.length());
+
+// задача 2
 
     }
 }
